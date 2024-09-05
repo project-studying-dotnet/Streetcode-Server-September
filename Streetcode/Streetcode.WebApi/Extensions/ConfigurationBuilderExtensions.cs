bc6b0d3fae93f7e2de322ae02de737621400b470
Yevhen Tkach <zhekatkach04@gmail.com>
2024-09-05 09:14:23 +0300

    refactor/2: remove sensitive data

---
+++ Streetcode/Streetcode.WebApi/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Streetcode.WebApi.Extensions
+{
+    public static class ConfigurationBuilderExtensions
+    {
+        public static IConfigurationBuilder ConfigureCustom(this IConfigurationBuilder builder, string environment)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables("STREETCODE_");
+
+            builder.AddUserSecrets<Program>();
+
+            return builder;
+        }
+    }
+}
