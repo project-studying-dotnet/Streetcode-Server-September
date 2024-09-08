3d6ec102f98040199b9949f8d4891b5477bc774c
Vita Kotenko <mialkovska.viktoria@gmail.com>
2024-09-08 20:14:56 +0300

    added test

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Streetcode/Fact/GetById/GetFactByIdHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Fact.GetById;
+
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using Streetcode.BLL.Dto.Streetcode.TextContent.Fact;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Streetcode.Fact.GetById;
+using Streetcode.DAL.Entities.Streetcode.TextContent;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+    public class GetFactByIdHandlerTests
+    {
+        private readonly Mock<IRepositoryWrapper> _repositoryWrapperMock;
+        private readonly Mock<IMapper> _mapperMock;
+        private readonly Mock<ILoggerService> _loggerMock;
+        private readonly GetFactByIdHandler _handler;
+
+        public GetFactByIdHandlerTests()
+        {
+            _repositoryWrapperMock = new Mock<IRepositoryWrapper>();
+            _mapperMock = new Mock<IMapper>();
+            _loggerMock = new Mock<ILoggerService>();
+            _handler = new GetFactByIdHandler(_repositoryWrapperMock.Object, _mapperMock.Object, _loggerMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsError_WhenFactNotExist()
+        {
+        // Arrange
+        var request = new GetFactByIdQuery(1);
+        _repositoryWrapperMock.Setup(repo => repo.FactRepository
+            .GetFirstOrDefaultAsync(It.IsAny<Expression<Func<Fact, bool>>>(), null))
+            .ReturnsAsync((Fact)null);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        Assert.Multiple(
+            () => Assert.True(result.IsFailed));
+
+    }
+}
