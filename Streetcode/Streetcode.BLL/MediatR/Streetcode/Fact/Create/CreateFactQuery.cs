2732ed6e1742ce9bcc9d9724ff13d96f0a236646
refokcer <refokcer@gmail.com>
2024-09-13 19:16:49 +0300

    Created MediatR for Streetcode / Fact / Create Query and Handler
    !! With HardCode for StreetcodeId !!
    !! Need fix it !!

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Create/CreateFactQuery.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.Dto.Streetcode.TextContent.Fact;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Fact.Create
+{
+    public record CreateFactQuery(FactDto fact) : IRequest<Result<FactDto>>;
+}
