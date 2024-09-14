a1f1b8efc5ad81e7c9b46a1cb047b766268431fb
refokcer <refokcer@gmail.com>
2024-09-14 13:17:22 +0300

    ImageId now nullable

--- Streetcode/Streetcode.BLL/Dto/Streetcode/TextContent/Fact/FactDto.cs
+++ Streetcode/Streetcode.BLL/Dto/Streetcode/TextContent/Fact/FactDto.cs
@@ -4,7 +4,7 @@ public class FactDto
 {
     public int Id { get; set; }
     public string Title { get; set; } = null!;
-    public int ImageId { get; set; }
+    public int? ImageId { get; set; }
     public string? FactContent { get; set; }
     public int StreetcodeId { get; set; }
 }
