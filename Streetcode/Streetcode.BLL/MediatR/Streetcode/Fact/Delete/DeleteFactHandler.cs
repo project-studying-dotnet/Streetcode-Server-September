06cf528a4f232c233742cbc7544320c7a87fff6e
refokcer <refokcer@gmail.com>
2024-09-14 12:12:10 +0300

    Created DeleteFactHandler with logic for delet fact by id

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Delete/DeleteFactHandler.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Newss.Delete
+{
+    public class DeleteFactHandler : IRequestHandler<DeleteFactCommand, Result<Unit>>
+    {
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly ILoggerService _logger;
+        public DeleteFactHandler(IRepositoryWrapper repositoryWrapper, ILoggerService logger)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _logger = logger;
+        }
+
+
+        public async Task<Result<Unit>> Handle(DeleteFactCommand request, CancellationToken cancellationToken)
+        {
+            int id = request.id;
+            var fact = await _repositoryWrapper.FactRepository.GetFirstOrDefaultAsync(f => f.Id == id);
+
+            if (fact == null)
+            {
+                string errorMsg = $"No fact found by entered Id - {id}";
+                _logger.LogError(request, errorMsg);
+                return Result.Fail(errorMsg);
+            }
+
+            /*if (fact.Image is not null)
+            {
+                _repositoryWrapper.ImageRepository.Delete(fact.Image);
+            }*/
+
+            _repositoryWrapper.FactRepository.Delete(fact);
+            var resultIsSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
+            if (resultIsSuccess)
+            {
+                return Result.Ok(Unit.Value);
+            }
+            else
+            {
+                string errorMsg = "Failed to delete fact";
+                _logger.LogError(request, errorMsg);
+                return Result.Fail(new Error(errorMsg));
+            }
+        }
+    }
+}
