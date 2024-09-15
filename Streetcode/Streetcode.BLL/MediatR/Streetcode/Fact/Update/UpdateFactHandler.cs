622be6ac5ece636f06eaf24b056eff88670810bb
refokcer <refokcer@gmail.com>
2024-09-15 18:31:00 +0300

    Create MediatR Need fix problem with StreetcodeId Create new Dto

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Update/UpdateFactHandler.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.Dto.News;
+using Streetcode.BLL.Dto.Streetcode.TextContent.Fact;
+using Streetcode.BLL.Interfaces.BlobStorage;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Newss.Update;
+using Streetcode.DAL.Entities.News;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FactEntety =  Streetcode.DAL.Entities.Streetcode.TextContent.Fact;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Fact.Update
+{
+    public class UpdateFactHandler : IRequestHandler<UpdateFactCommand, Result<FactDto>>
+    {
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly IMapper _mapper;
+        // private readonly IBlobService _blobSevice;
+        private readonly ILoggerService _logger;
+        public UpdateFactHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper, IBlobService blobService, ILoggerService logger)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _mapper = mapper;
+            // _blobSevice = blobService;
+            _logger = logger;
+        }
+
+        public async Task<Result<FactDto>> Handle(UpdateFactCommand request, CancellationToken cancellationToken)
+        {
+            var fact = _mapper.Map<FactEntety>(request.Fact);
+
+            if (fact is null)
+            {
+                const string errorMsg = $"Cannot convert null to fact";
+                _logger.LogError(request, errorMsg);
+                return Result.Fail(new Error(errorMsg));
+            }
+
+            var response = _mapper.Map<FactDto>(fact);
+
+            _repositoryWrapper.FactRepository.Update(fact);
+            bool resultIsSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
+
+            if (resultIsSuccess)
+            {
+                return Result.Ok(response);
+            }
+            else
+            {
+                const string errorMsg = $"Failed to update fact";
+                _logger.LogError(request, errorMsg);
+                return Result.Fail(new Error(errorMsg));
+            }
+        }
+    }
+}
