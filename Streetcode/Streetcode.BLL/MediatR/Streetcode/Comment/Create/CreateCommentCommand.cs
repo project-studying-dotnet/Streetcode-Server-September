dfbc97a3279d2f1879b452740096188241d89414
refokcer <refokcer@gmail.com>
2024-09-22 23:46:43 +0300

    Added Handler with command

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/Comment/Create/CreateCommentCommand.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Comment/Create/CreateCommentCommand.cs
@@ -4,5 +4,5 @@ using Streetcode.BLL.Dto.Streetcode.TextContent.Comment;
 
 namespace Streetcode.BLL.MediatR.Streetcode.Comment.Create
 {
-    public record CreateCommentCommand(CommentCreateDto Fact) : IRequest<Result<CommentDto>>;
+    public record CreateCommentCommand(CommentCreateDto Comment) : IRequest<Result<CommentDto>>;
 }
