8808377582d46d3f17b47039d17af7c303b6b3a2
refokcer <refokcer@gmail.com>
2024-09-22 23:17:23 +0300

    Added comand for create comment

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Comment/Create/CreateCommentCommand.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.Dto.Streetcode.TextContent.Comment;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Comment.Create
+{
+    public record CreateCommentCommand(CommentCreateDto Fact) : IRequest<Result<CommentDto>>;
+}
