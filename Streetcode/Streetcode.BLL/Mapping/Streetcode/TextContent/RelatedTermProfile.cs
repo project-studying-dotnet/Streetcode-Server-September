2fcdf996a0c1f2ae0334aee700b363e8589a25df
Yevhen Tkach <zhekatkach04@gmail.com>
2024-09-22 22:01:05 +0300

    feat/165/ write unit tests for handler

--- Streetcode/Streetcode.BLL/Mapping/Streetcode/TextContent/RelatedTermProfile.cs
+++ Streetcode/Streetcode.BLL/Mapping/Streetcode/TextContent/RelatedTermProfile.cs
@@ -11,14 +11,6 @@ public class RelatedTermProfile : Profile
     {
         CreateMap<RelatedTerm, RelatedTermDto>().ReverseMap();
         CreateMap<RelatedTermCreateDto, RelatedTerm>();
-        // CreateMap<RelatedTerm, RelatedTermFullDto>()
-        //     .ForPath(dto => dto.Id, config => config.MapFrom(entity => entity.Id))
-        //     .ForPath(dto => dto.Word, config => config.MapFrom(entity => entity.Word))
-        //     .ForPath(dto => dto.TermDto, config => config.MapFrom(entity => entity.Term));
-        // CreateMap<RelatedTerm, RelatedTermFullDto>()
-        //     .ForMember(dto => dto.Id, config => config.MapFrom(entity => entity.Id))
-        //     .ForMember(dto => dto.Word, config => config.MapFrom(entity => entity.Word))
-        //     .ForMember(dto => dto.TermDto, config => config.MapFrom(entity => entity.Term));
         CreateMap<RelatedTerm, RelatedTermFullDto>()
             .ForCtorParam("Id", opt => opt.MapFrom(src => src.Id))
             .ForCtorParam("Word", opt => opt.MapFrom(src => src.Word))
