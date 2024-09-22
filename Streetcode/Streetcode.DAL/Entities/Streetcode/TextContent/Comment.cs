040cf63bd4bb60fac8a5976e9fa7386dc47b4731
refokcer <refokcer@gmail.com>
2024-09-22 21:57:02 +0300

    Created Comment Entetty

---
+++ Streetcode/Streetcode.DAL/Entities/Streetcode/TextContent/Comment.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Streetcode.DAL.Entities.Streetcode.TextContent;
+
+[Table("comments", Schema = "streetcode")]
+public class Comment
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(1000)]
+    public string? CommentContent { get; set; }
+
+    [Required]
+    public int StreetcodeId { get; set; }
+
+    public StreetcodeContent? Streetcode { get; set; }
+
+    public int? ParentCommentId { get; set; }
+
+    [Required]
+    public int UserId { get; set; }
+
+    // public User? User { get; set; }
+
+    [Required]
+    public DateTime DateCreated { get; set; }
+}
