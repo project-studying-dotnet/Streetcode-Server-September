c9c37aad7eb800913a4a3e87bf2691a393ca1fc8
refokcer <refokcer@gmail.com>
2024-09-22 23:22:17 +0300

    Add mapping

---
+++ Streetcode/Streetcode.BLL/Mapping/Streetcode/TextContent/CommentProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Streetcode.BLL.Dto.Streetcode.TextContent.Comment;
+using Streetcode.DAL.Entities.Streetcode.TextContent;
+
+namespace Streetcode.BLL.Mapping.Streetcode.TextContent;
+
+public class CommentProfile : Profile
+{
+    public CommentProfile()
+    {
+        CreateMap<Comment, CommentDto>().ReverseMap();
+        CreateMap<Comment, CommentCreateDto>().ReverseMap();
+    }
+}
