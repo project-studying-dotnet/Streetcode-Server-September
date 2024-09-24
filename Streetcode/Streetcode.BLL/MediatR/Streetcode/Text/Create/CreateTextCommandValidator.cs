44062b3a5679e99398615c4f9f0fe46ae2cbec37
Yevhen Tkach <zhekatkach04@gmail.com>
2024-09-24 15:02:59 +0300

    feat/81/ create CreateTextCommand and CreateTextCommandValidator

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Text/Create/CreateTextCommandValidator.cs
@@ -0,0 +1,31 @@
+using FluentValidation;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Text.Create;
+
+public class CreateTextCommandValidator: AbstractValidator<CreateTextCommand>
+{
+    private const int TitleMaxLength = 300;
+    private const int TextContentMaxLength = 15000;
+    private const int AdditionalTextMaxLength = 200;
+    
+    public CreateTextCommandValidator()
+    {
+        RuleFor(x => x.TextCreateDto.Title)
+            .NotEmpty()
+            .WithMessage("Text title is required")
+            .MaximumLength(TitleMaxLength)
+            .WithMessage($"Text title must not exceed {TitleMaxLength} chars");
+        
+        RuleFor(x => x.TextCreateDto.TextContent)
+            .NotEmpty()
+            .WithMessage("Text content is required")
+            .MaximumLength(TextContentMaxLength)
+            .WithMessage($"Text content must not exceed {TextContentMaxLength} chars");
+        
+        RuleFor(x => x.TextCreateDto.AdditionalText)
+            .NotEmpty()
+            .WithMessage("Text additional text is required")
+            .MaximumLength(AdditionalTextMaxLength)
+            .WithMessage($"Text additional text must not exceed {AdditionalTextMaxLength} chars");
+    }
+}
