f50bf0c3f4cf131ea2d2a3ca1c8c8d6d0c7317fa
refokcer <refokcer@gmail.com>
2024-09-25 21:16:34 +0300

    Create endPoint and mediatr fot GetByStreetcodeId  for comment

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Comment/GetByStreetcodeId/GetCommentByStreetcodeIdHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Streetcode.BLL.Dto.Streetcode.TextContent.Comment;
+using Streetcode.BLL.Exceptions.CustomExceptions;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Streetcode.Comment.GetByStreetcodeId;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Fact.GetByStreetcodeId;
+
+public class GetCommentByStreetcodeIdHandler : IRequestHandler<GetCommentByStreetcodeIdQuery, Result<IEnumerable<CommentDto>>>
+{
+    private readonly IMapper _mapper;
+    private readonly IRepositoryWrapper _repositoryWrapper;
+    private readonly ILoggerService _logger;
+
+    public GetCommentByStreetcodeIdHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper, ILoggerService logger)
+    {
+        _repositoryWrapper = repositoryWrapper;
+        _mapper = mapper;
+        _logger = logger;
+    }
+
+    public async Task<Result<IEnumerable<CommentDto>>> Handle(GetCommentByStreetcodeIdQuery request, CancellationToken cancellationToken)
+    {
+        var comment = await _repositoryWrapper.CommentRepository
+            .GetAllAsync(c => c.StreetcodeId == request.StreetcodeId);
+
+        if (comment is null)
+            throw new CustomException($"Cannot find any comment by the streetcode id: {request.StreetcodeId}", StatusCodes.Status204NoContent);
+
+        var response = _mapper.Map<IEnumerable<CommentDto>>(comment);
+        return Result.Ok(response);
+    }
+}
