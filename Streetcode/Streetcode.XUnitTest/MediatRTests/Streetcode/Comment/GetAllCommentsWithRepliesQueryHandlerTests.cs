5bf4f7241ceb9caeb8a2a2c6fe782ccf45f303a1
Artem Riabushenko <artemryabushenko@gmail.com>
2024-09-26 17:34:22 +0300

    additional unit tests

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Streetcode/Comment/GetAllCommentsWithRepliesQueryHandlerTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Streetcode/Comment/GetAllCommentsWithRepliesQueryHandlerTests.cs
@@ -105,5 +105,86 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.Comment
             Assert.True(result.IsSuccess);
             Assert.Empty(result.Value);
         }
+
+        [Fact]
+        public async Task Handle_ReturnsNull_WhenMappingReturnsNull()
+        {
+            // Arrange
+            var comments = new List<DAL.Entities.Streetcode.TextContent.Comment>
+            {
+                new DAL.Entities.Streetcode.TextContent.Comment
+                {
+                    Id = 1,
+                    CommentContent = "Parent Comment",
+                    Replies = new List<DAL.Entities.Streetcode.TextContent.Comment>
+                    {
+                        new DAL.Entities.Streetcode.TextContent.Comment { Id = 2, CommentContent = "Reply 1" },
+                        new DAL.Entities.Streetcode.TextContent.Comment { Id = 3, CommentContent = "Reply 2" }
+                    }
+                }
+            };
+
+            _mockRepo.Setup(repo => repo.CommentRepository.GetAllAsync(
+                It.IsAny<Expression<Func<DAL.Entities.Streetcode.TextContent.Comment, bool>>>(),
+                It.IsAny<Func<IQueryable<DAL.Entities.Streetcode.TextContent.Comment>, IIncludableQueryable<DAL.Entities.Streetcode.TextContent.Comment, object>>>())).ReturnsAsync(comments);
+
+            _mockMapper.Setup(mapper => mapper.Map<List<CommentWithRepliesDto>>(comments))
+                .Returns((List<CommentWithRepliesDto>)null);
+
+            // Act
+            var result = await _handler.Handle(new GetAllCommentsWithRepliesQuery(), CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.Null(result.Value);
+        }
+
+        [Fact]
+        public async Task Handle_ExecutesRepositoryAndMapper_Correctly()
+        {
+            // Arrange
+            var comments = new List<DAL.Entities.Streetcode.TextContent.Comment>
+            {
+                new DAL.Entities.Streetcode.TextContent.Comment
+                {
+                    Id = 1,
+                    CommentContent = "Parent Comment",
+                    Replies = new List<DAL.Entities.Streetcode.TextContent.Comment>
+                    {
+                        new DAL.Entities.Streetcode.TextContent.Comment { Id = 2, CommentContent = "Reply 1" },
+                        new DAL.Entities.Streetcode.TextContent.Comment { Id = 3, CommentContent = "Reply 2" }
+                    }
+                }
+            };
+
+            _mockRepo.Setup(repo => repo.CommentRepository.GetAllAsync(
+                It.IsAny<Expression<Func<DAL.Entities.Streetcode.TextContent.Comment, bool>>>(),
+                It.IsAny<Func<IQueryable<DAL.Entities.Streetcode.TextContent.Comment>, IIncludableQueryable<DAL.Entities.Streetcode.TextContent.Comment, object>>>())).ReturnsAsync(comments);
+
+            _mockMapper.Setup(mapper => mapper.Map<List<CommentWithRepliesDto>>(comments))
+                .Returns(new List<CommentWithRepliesDto>
+                {
+                    new CommentWithRepliesDto
+                    {
+                        Id = 1,
+                        CommentContent = "Parent Comment",
+                        Replies = new List<CommentDto>
+                        {
+                            new CommentDto { Id = 2, CommentContent = "Reply 1" },
+                            new CommentDto { Id = 3, CommentContent = "Reply 2" }
+                        }
+                    }
+                });
+
+            // Act
+            var result = await _handler.Handle(new GetAllCommentsWithRepliesQuery(), CancellationToken.None);
+
+            // Assert
+            _mockRepo.Verify(repo => repo.CommentRepository.GetAllAsync(
+                It.IsAny<Expression<Func<DAL.Entities.Streetcode.TextContent.Comment, bool>>>(),
+                It.IsAny<Func<IQueryable<DAL.Entities.Streetcode.TextContent.Comment>, IIncludableQueryable<DAL.Entities.Streetcode.TextContent.Comment, object>>>()), Times.Once);
+
+            _mockMapper.Verify(mapper => mapper.Map<List<CommentWithRepliesDto>>(comments), Times.Once);
+        }
     }
 }
