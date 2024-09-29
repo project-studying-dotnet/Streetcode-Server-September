990a2219a3873bcf73e2e1705aacccf4304cf776
Yevhen Tkach <zhekatkach04@gmail.com>
2024-09-29 13:45:25 +0300

    feat/209/remove primary constructor

--- Streetcode/Streetcode.BLL/Services/Cache/CacheService.cs
+++ Streetcode/Streetcode.BLL/Services/Cache/CacheService.cs
@@ -3,56 +3,62 @@ using Microsoft.Extensions.Caching.Distributed;
 using Newtonsoft.Json;
 using Streetcode.BLL.Exceptions.CustomExceptions;
 
-namespace Streetcode.BLL.Services.Cache
-{
-    public class CacheService(IDistributedCache distributedCache) : ICacheService
-    {
-        public async Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default)
-        {
-            string? data = await distributedCache.GetStringAsync(key, cancellationToken);
-        
-            if (data is null)
-            {
-                return default;
-            }
+namespace Streetcode.BLL.Services.Cache;
 
-            return JsonConvert.DeserializeObject<T>(data) ?? throw new CustomException(
-                "Error while deserializing cached value",
-                StatusCodes.Status500InternalServerError);
+public class CacheService : ICacheService
+{
+    private readonly IDistributedCache _distributedCache;
+
+    public CacheService(IDistributedCache distributedCache)
+    {
+        _distributedCache = distributedCache;
+    }
+
+    public async Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default)
+    {
+        string? data = await _distributedCache.GetStringAsync(key, cancellationToken);
+        
+        if (data is null)
+        {
+            return default;
         }
 
-        public async Task<T> GetAsync<T>(string key, Func<Task<T>> factory, CancellationToken cancellationToken = default,
-            TimeSpan? absoluteExpireTime = null, TimeSpan? unusedExpireTime = null)
-        {
-            T? cachedData = await GetAsync<T>(key, cancellationToken);
-        
-            if (cachedData is not null)
-            {
-                return cachedData;
-            }
-            
-            cachedData = await factory();
-        
-            await SetAsync(key, cachedData, cancellationToken);
+        return JsonConvert.DeserializeObject<T>(data) ?? throw new CustomException(
+            "Error while deserializing cached value",
+            StatusCodes.Status500InternalServerError);
+    }
 
+    public async Task<T> GetAsync<T>(string key, Func<Task<T>> factory, CancellationToken cancellationToken = default,
+        TimeSpan? absoluteExpireTime = null, TimeSpan? unusedExpireTime = null)
+    {
+        T? cachedData = await GetAsync<T>(key, cancellationToken);
+        
+        if (cachedData is not null)
+        {
             return cachedData;
         }
-
-        public async Task SetAsync<T>(
-            string key, 
-            T data,
-            CancellationToken cancellationToken = default,
-            TimeSpan? absoluteExpireTime = null, 
-            TimeSpan? unusedExpireTime = null)
-        {
-            var options = new DistributedCacheEntryOptions
-            {
-                AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromMinutes(5),
-                SlidingExpiration = unusedExpireTime
-            };
+            
+        cachedData = await factory();
         
-            string serializedData = JsonConvert.SerializeObject(data);
-            await distributedCache.SetStringAsync(key, serializedData, options, cancellationToken);
-        }
+        await SetAsync(key, cachedData, cancellationToken);
+
+        return cachedData;
+    }
+
+    public async Task SetAsync<T>(
+        string key, 
+        T data,
+        CancellationToken cancellationToken = default,
+        TimeSpan? absoluteExpireTime = null, 
+        TimeSpan? unusedExpireTime = null)
+    {
+        var options = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromMinutes(5),
+            SlidingExpiration = unusedExpireTime
+        };
+        
+        string serializedData = JsonConvert.SerializeObject(data);
+        await _distributedCache.SetStringAsync(key, serializedData, options, cancellationToken);
     }
 }
