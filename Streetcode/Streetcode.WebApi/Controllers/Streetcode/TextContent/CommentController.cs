a37ab51b99f286f0179d8e27081d8f62d7293781
Yevhen Tkach <zhekatkach04@gmail.com>
2024-10-01 20:34:11 +0300

    feat/208/ add Authorize(Roles = Admin) attribute

--- Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/CommentController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/CommentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.Dto.Streetcode.TextContent.Comment;
 using Streetcode.BLL.MediatR.Streetcode.Comment.Create;
 using Streetcode.BLL.MediatR.Streetcode.Comment.Delete;
@@ -17,6 +18,7 @@ public class CommentController : BaseApiController
     }
 
     [HttpDelete("{id:int}")]
+    [Authorize(Roles = "Admin")]
     public async Task<IActionResult> Delete([FromRoute] int id)
     {
         return HandleResult(await Mediator.Send(new DeleteCommentCommand(id)));
