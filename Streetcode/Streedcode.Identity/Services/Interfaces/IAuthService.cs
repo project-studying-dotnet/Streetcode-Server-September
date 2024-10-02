a8a41915ee425d27555673c003a4856cdbd8ffe6
MishaHMK <gumeniukmich@gmail.com>
2024-10-02 20:15:57 +0300

    Logout

--- Streetcode/Streedcode.Identity/Services/Interfaces/IAuthService.cs
+++ Streetcode/Streedcode.Identity/Services/Interfaces/IAuthService.cs
@@ -4,5 +4,6 @@ namespace Streetcode.Identity.Services.Interfaces;
 public interface IAuthService
 {
    public Task<LoginResultDto> LoginAsync(LoginDto loginDto, CancellationToken cancellationToken);
-   public Task<string> RefreshJwtToken(int userIdm, CancellationToken cancellationToken);
+   public Task LogoutAsync(CancellationToken cancellationToken);
+   public Task<string> RefreshJwtToken(CancellationToken cancellationToken);
 }
