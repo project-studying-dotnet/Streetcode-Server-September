e7e8cabb4e02fe602297daa3111f941041e80614
refokcer <refokcer@gmail.com>
2024-10-13 17:02:52 +0300

    Done task

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/GetFavorites/GetFavouritesListHandler.cs
@@ -0,0 +1,33 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Streetcode.BLL.Exceptions.CustomExceptions;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.GetFavouritesList;
+
+public class GetFavouritesListHandler : IRequestHandler<GetFavouritesListQuery, Result<List<int>>>
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private const string CookieName = "favouritesStreetcodes";
+
+    public GetFavouritesListHandler(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public Task<Result<List<int>>> Handle(GetFavouritesListQuery request, CancellationToken cancellationToken)
+    {
+        var context = _httpContextAccessor.HttpContext ?? throw new CustomException("No HttpContext in request", StatusCodes.Status400BadRequest);
+
+        var favouritesCookie = context.Request.Cookies[CookieName];
+        var favourites = new List<int>();
+
+        if (!string.IsNullOrEmpty(favouritesCookie))
+        {
+            // Разбираем существующий список избранного из куки
+            favourites = favouritesCookie.Split(',').Select(int.Parse).ToList();
+        }
+
+        return Task.FromResult(Result.Ok(favourites));
+    }
+}
